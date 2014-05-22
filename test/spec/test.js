6e001058451f93b9d6d9a497d5bc543f0ac4b17f
William Creighton <william.creighton@gmail.com>
2014-05-22 15:18:56 -0400

    Added Roll-Result Function

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,54 +3,54 @@
 (function() {
 	'use strict';
 
-	describe('Give it some context', function() {
-		describe('maybe a bit more context here', function() {
-			it('should be the expected value', function() {
-				var answer = 2;
-				expect(2).to.equal(answer);
-				expect(answer).to.be.a("number");
-			});
+	// describe('Give it some context', function() {
+	// 	describe('maybe a bit more context here', function() {
+	// 		it('should be the expected value', function() {
+	// 			var answer = 2;
+	// 			expect(2).to.equal(answer);
+	// 			expect(answer).to.be.a("number");
+	// 		});
 
-			it('should be a number', function() {
-				var answer = 2;
-				expect(2).to.equal(answer);
-				expect(answer).to.be.a("number");
-			});
+	// 		it('should be a number', function() {
+	// 			var answer = 2;
+	// 			expect(2).to.equal(answer);
+	// 			expect(answer).to.be.a("number");
+	// 		});
 
-		});
-	});
-
-
-
-	describe('square function', function() {
-		it('should multiply its argument by itself', function() {
-			var answer = square(2);
-			expect(answer).to.equal(4);
-		});
-
-		it('should throw errors for non Number types', function() {
-
-			var erroneousSquare = function() {
-				square('2');
-			}
-
-			expect(erroneousSquare).to.throw(Error);
-		});
-
-		it('should throw an error when no args are provided', function() {
-			expect(square).to.throw(Error);
-		});
-	});
+	// 	});
+	// });
 
 
-	describe('error checker checker', function() {
-		it('should check to see if the testing mechanism is working', function() {
 
-			var testingMech = testingMechTester(2);
+	// describe('square function', function() {
+	// 	it('should multiply its argument by itself', function() {
+	// 		var answer = square(2);
+	// 		expect(answer).to.equal(4);
+	// 	});
 
-			expect(testingMech).to.equal(2);
-		});
-	});
+	// 	it('should throw errors for non Number types', function() {
+
+	// 		var erroneousSquare = function() {
+	// 			square('2');
+	// 		}
+
+	// 		expect(erroneousSquare).to.throw(Error);
+	// 	});
+
+	// 	it('should throw an error when no args are provided', function() {
+	// 		expect(square).to.throw(Error);
+	// 	});
+	// });
+
+
+	// describe('error checker checker', function() {
+	// 	it('should check to see if the testing mechanism is working', function() {
+
+	// 		var testingMech = testingMechTester(2);
+
+	// 		expect(testingMech).to.equal(2);
+	// 	});
+	// });
 
 
 	describe("Create Die Button", function() {
