b163234f3fc5c80706854f8759a142e9786be197
William Creighton <william.creighton@gmail.com>
2014-05-24 14:22:20 -0400

    Testing KB Dice Roll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,14 +8,14 @@ function Die (sides) {
   }
 }
  
-$('.create-die.button').click(function(){
-  var sides = $('.dice-input.create-die').val();
-  gameDie = new Die(sides);
-  console.log(sides);
-  $('.intro').remove();
-  $('#tridiv').addClass('activate');
-  $('.roll-result').html(gameDie.roll());
-});
+// $('.create-die.button').click(function(){
+//   var sides = $('.dice-input.create-die').val();
+//   gameDie = new Die(sides);
+//   console.log(sides);
+//   $('.intro').remove();
+//   $('#tridiv').addClass('activate');
+//   $('.roll-result').html(gameDie.roll());
+// });
 
 // function renderRandomDieValue () {
 //   $('.roll-result').html("You rolled a " + gameDie.roll() ".")
@@ -25,3 +25,11 @@ $('.create-die.button').click(function(){
 //   $('.roll-result').addClass('active')
 // }
 
+$('.create-die.button').click(function() {
+	// $('.create-dice.roll-six-sides').remove();
+	$('.shape.cuboid-1.cub-1.cub-a').addClass("activate");
+	var sides = 6;
+	gameDie = new Die(sides);
+	console.log(sides);
+	$('.result.cub-a').text(gameDie.roll()).addClass("activate");
+});
