5644a9d2d291c8a71a27a683b8e54e9d8f72a22d
Kevin Bost <kevbost@gmail.com>
2014-07-30 10:09:25 -0400

    rounded numbers, added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+##Roll Dice!
+
+This app was made as a collaborative project at the Iron Yard. It is a dice rolling visualizer that is made with Jquery and styled using tridiv.coms tools to create the 3d cube.
+
+You have the option to roll a 6 sided die, or input a number which would represent the outcome of the roll of a die with as many sides as you want.
+
+[http://kevbost.github.io/roll-dice/](http://kevbost.github.io/roll-dice/)
+
+##Assets:
+Javascript - jQuery - HTML - CSS
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,45 +8,27 @@ function Die(sides) {
 	this.roll = function() {
 		var result = Math.floor(Math.random() * this.sides + 1);
 		console.log('rolled 1d' + this.sides, " for ", result);
-		return result;
+		return Math.floor(result);
 	};
 }
-
-// $('.create-die.button').click(function(){
-//   var sides = $('.dice-input.create-die').val();
-//   gameDie = new Die(sides);
-//   console.log(sides);
-//   $('.intro').remove();
-//   $('#tridiv .roll-result').addClass('activate');
-//   $('.roll-result').html(gameDie.roll());
-// });
-
-// function renderRandomDieValue () {
-//   $('.roll-result').html("You rolled a " + gameDie.roll() ".")
-// }
-
-// function showRollResult() {
-//   $('.roll-result').addClass('active')
-// }
-
-
 
 $('.create-die1.button1').click(function() {
 	// $('.create-dice.roll-six-sides').remove();
 	var sides = 6;
 	gameDie = new Die(sides);
+	var roll = gameDie.roll()
 	$('.shape.cuboid-1.cub-1').addClass("activate opacity");
 	$('.shape.cuboid-1.cub-1').removeClass("reset");
 	console.log(sides + 'die 1');
 	$('.intro').remove();
 	// $('.roll-result').html('<h2 class="h2Class">' + gameDie.roll() + '</h2>').addClass("activate");
-	$('.ft').html('<h3>' + gameDie.roll() / 1 + '</h3>').addClass("activate");
-	$('.bk').html('<h3>' + gameDie.roll() / 2 + '</h3>').addClass("activate");
-	$('.rt').html('<h3>' + gameDie.roll() / 3 + '</h3>').addClass("activate");
-	$('.lt').html('<h3>' + gameDie.roll() / 4 + '</h3>').addClass("activate");
-	$('.bm').html('<h3>' + gameDie.roll() / 5 + '</h3>').addClass("activate");
+	$('.ft').html('<h3>' + Math.floor((roll / 1) + 1) + '</h3>').addClass("activate");
+	$('.bk').html('<h3>' + Math.floor((roll / 2) + 1) + '</h3>').addClass("activate");
+	$('.rt').html('<h3>' + Math.floor((roll / 3) + 1) + '</h3>').addClass("activate");
+	$('.lt').html('<h3>' + Math.floor((roll / 4) + 1) + '</h3>').addClass("activate");
+	$('.bm').html('<h3>' + Math.floor((roll / 5) + 1) + '</h3>').addClass("activate");
 	$('.tp').html('<h3>' + gameDie.sides + '</h3>').addClass("activate");
-	console.log("removing activate adding reset");
+	console.log("removing activate adding reset")
 
 	var resetting = function() {
 		$('.shape.cuboid-1.cub-1').removeClass("activate").addClass("reset");
@@ -59,17 +41,18 @@ $('.create-die1.button1').click(function() {
 $('.create-die2.button2').click(function() {
 	var sides = $('.dice-input.create-die').val();
 	gameDie = new Die(sides);
+	var roll = gameDie.roll()
 	$('.shape.cuboid-1.cub-1').addClass("activate opacity").removeClass("reset");
 	// $('.roll-result').removeClass("reset");
 	// $('.face.ft').html();
 	console.log(sides + 'die 2');
 	$('.intro').remove();
 	// $('.roll-result').html('<h2 class="h2Class">' + gameDie.roll() + '</h2>').addClass("activate");
-	$('.ft').html('<h3>' + gameDie.roll() / 1 + '</h3>').addClass("activate");
-	$('.bk').html('<h3>' + gameDie.roll() / 2 + '</h3>').addClass("activate");
-	$('.rt').html('<h3>' + gameDie.roll() / 3 + '</h3>').addClass("activate");
-	$('.lt').html('<h3>' + gameDie.roll() / 4 + '</h3>').addClass("activate");
-	$('.bm').html('<h3>' + gameDie.roll() / 5 + '</h3>').addClass("activate");
+	$('.ft').html('<h3>' + Math.floor(roll / 1) + '</h3>').addClass("activate");
+	$('.bk').html('<h3>' + Math.floor(roll / 2) + '</h3>').addClass("activate");
+	$('.rt').html('<h3>' + Math.floor(roll / 3) + '</h3>').addClass("activate");
+	$('.lt').html('<h3>' + Math.floor(roll / 4) + '</h3>').addClass("activate");
+	$('.bm').html('<h3>' + Math.floor(roll / 5) + '</h3>').addClass("activate");
 	$('.tp').html('<h3>' + gameDie.sides + '</h3>').addClass("activate");
 	$('.dice-input.create-die').val('');
 	console.log("removing activate adding reset");
